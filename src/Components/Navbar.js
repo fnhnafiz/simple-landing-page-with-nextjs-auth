15865ce66eddd9df048a7800d8aceebf464b5411
fnhnafiz <nafsinsifan2k19@gmail.com>
2025-01-23 14:47:32 +0600

    update code for css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,7 @@ const NavbarPage = async () => {
   // console.log(user);
   return (
     <div className="bg-lime-500 px-8">
-      <nav className="container mx-auto flex gap-8 flex-wrap md:flex-nowrap md:justify-between items-center py-4 text-white">
+      <nav className="container lili mx-auto  text-white">
         <div>
           <h1 className="px-4 py-2 rounded-lg font-semibold border">WEB DEV</h1>
         </div>
